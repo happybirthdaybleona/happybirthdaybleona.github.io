f9d8580662e00dbbcdf778ff30e893059e73b2a7
Kastriot Kastrati <kastriotk@auk.local>
2019-05-17 22:46:39 +0200

    all

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  let audio = document.getElementsByClassName('mbox')[0];
+  let audiomain = document.getElementById('audiomain');
+
+  audio.addEventListener('click', function(){
+    audiomain.play();
+    audiomain.volume = 0.1;
+  });
+
+});
